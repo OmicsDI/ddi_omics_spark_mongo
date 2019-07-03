758eca94bb6d9ec092de0ef86d5b315cf3ea2360
Gaurhari <gaurharidass@gmail.com>
2019-07-03 15:59:17 +0100

    updated spark context info to retrieve

---
+++ src/main/scala/org/omics/sparkop/SparkInfo.scala
@@ -0,0 +1,38 @@
+package org.omics.sparkop
+
+import org.apache.hadoop.fs.LocalFileSystem
+import org.apache.hadoop.hdfs.DistributedFileSystem
+import org.apache.spark.sql.{DataFrame, SQLContext, SparkSession}
+import org.omics.utils.Constants
+
+object SparkInfo {
+
+  def getSparkSession():SparkSession = {
+    val sparkSession = SparkSession.builder()
+      .master(Constants.sparkMasterUrl)
+      .appName(Constants.sparkAppName)
+      .config(Constants.mongoInputUriKey, Constants.prodMongoUri)
+      .config(Constants.mongOutUriKey, Constants.prodMongoUri)
+      .config("fs.hdfs.impl",DistributedFileSystem.)
+      .getOrCreate()
+    sparkSession.conf.set("spark.driver.memory","6g")
+    sparkSession.conf.set("spark.executor.memory", "5g")
+    sparkSession
+  }
+
+  def getSqlContext():SQLContext = {
+    getSparkSession().sqlContext
+  }
+
+  def readCsv(filePath:String,sc:SQLContext): DataFrame =
+  {
+    val csvData = sc.read.format("csv")
+      .option("header", "true")//.schema(schema)
+      .option("inferSchema",true)
+      .option("delimiter", "\t")
+      .load(filePath)
+
+    csvData
+  }
+
+}
